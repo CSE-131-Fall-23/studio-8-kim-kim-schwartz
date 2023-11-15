310cdb097165b842f841f91498105c20eebeb7f7
ariannaschwartz <ariannaschwartz@localhost>
2023-11-15 14:17:23 -0600

    131 Studio 8

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -1,17 +1,26 @@
 package studio8;
 
 public class MultipleChoiceQuestion extends Question {
+	// unique instance variables
+	private String[] choices;
+	
 	
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
-		//FIXME
+	
+		super(prompt, answer, points); 
 		
-		//Call the super class constructor, then create and set
-		//instance variables for any values that aren't handled
-		//by the base class
+		this.choices = choices; // adding on to the constructor where the base class doesn't have it
+		
+		
 	}
 	
 	public void displayPrompt() {
 		//FIXME
+		super.displayPrompt(); // from the base class
+		for (int i = 0; i< choices.length; i++) {
+			System.out.println( (i+1) + ". "+ choices[i]);
+		}
+		return;
 		
 		/*
 		 * Use the method from the base class to display the prompt,
